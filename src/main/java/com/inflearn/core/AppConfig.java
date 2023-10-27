8d6f2e129ea95ac83b7457640fce8363986db181
ansdmswl0722 <ansdmswl0722@gmail.com>
2023-10-27 15:29:14 +0900

    MemberApp에 스프링 컨테이너 적용

--- src/main/java/com/inflearn/core/AppConfig.java
+++ src/main/java/com/inflearn/core/AppConfig.java
@@ -8,19 +8,26 @@ import com.inflearn.core.member.MemberServiceImpl;
 import com.inflearn.core.member.MemoryMemberRepository;
 import com.inflearn.core.order.OrderService;
 import com.inflearn.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(),discountPolicy());
     }
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
